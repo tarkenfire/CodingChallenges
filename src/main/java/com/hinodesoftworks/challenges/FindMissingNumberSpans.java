f4362456cd3609a1537977ea85501a4935489435
Michael Mancuso <tarkenfire@tarkenfire.com>
2016-12-11 12:34:18 -0500

    Inital Commit

---
+++ src/main/java/com/hinodesoftworks/challenges/FindMissingNumberSpans.java
@@ -0,0 +1,12 @@
+package com.hinodesoftworks.challenges;
+
+import com.hinodesoftworks.core.Testable;
+
+public class FindMissingNumberSpans implements Testable {
+
+	public String runTest() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
